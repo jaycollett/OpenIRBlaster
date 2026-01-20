4195eb89212a700ce36949ced0d1a8f80a2bbfc6
Jay Collett <486430+jaycollett@users.noreply.github.com>
2026-01-20 14:16:20 -0500

    Fix options flow init

--- custom_components/openirblaster/config_flow.py
+++ custom_components/openirblaster/config_flow.py
@@ -280,7 +280,7 @@ class OpenIRBlasterOptionsFlow(config_entries.OptionsFlow):
 
     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
         """Initialize options flow."""
-        self.config_entry = config_entry
+        super().__init__(config_entry)
         self._selected_code_id: str | None = None
         self._selected_code_name: str | None = None
 
